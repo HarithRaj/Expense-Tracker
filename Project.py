81d19935c4e0ab3b1b8578b86f66d7ce6ead6673
Harith Raj <147489169+HarithRaj@users.noreply.github.com>
2024-01-29 14:08:58 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,9 +1,9 @@
 import os
-import datetime     
+import datetime
+import time     
 from tabulate import tabulate
 
 table=[]
-No_of_Expenses=int(input("Enter the number of Expenses = "))
 
 def expense():  
     Total_expense=0
@@ -35,8 +35,32 @@ def expense():
     list_of_details=[i+1,date,Paid,Payment_Method,Expense_category,Expense,Total_expense]
     table.append(list_of_details)
 
+def func():
+    input('PRESS ENTER KEY TO VIEW OBJECTIVES')
+    print("OBJECTIVE")
+    print()
+    print("The objective of an expense tracker is to help individuals or organizations effectively manage their finances by keeping track of their expenses. Here are some specific objectives and benefits:")
+    print()
+    print("1.Financial Awareness: An expense tracker allows users to understand where their money is going. By categorizing expenses, users can identify areas where they are \n overspending and make necessary adjustments to their budget.")
+    print()
+    print("2.Budgeting: Expense trackers help users set and stick to a budget by providing insights into their spending habits. Users can allocate funds to different categories and track their progress towards staying within budget limits.")
+    print()
+    print("3.Identifying Trends: With historical expense data, users can identify spending trends over time. This information can be valuable for making informed financial\n decisions and planning for the future.")
+    print()
+    print("4.Financial Goal Setting: Expense trackers can help users set financial goals and track their progress towards achieving them. Whether it's saving for a vacation, \n paying off debt, or building an emergency fund, users can monitor their financial goals and adjust their spending habits accordingly.")
+    print()
+    print("5.Tax Preparation: Keeping track of expenses throughout the year can simplify the process of filing taxes. Users can easily access detailed records of their\n expenditures, making it easier to claim deductions and maximize tax savings.")
+    print()
+    print("6.Financial Accountability: For businesses or organizations, expense trackers promote financial accountability among employees by requiring them to document and \n justify their expenses. This helps prevent misuse of funds and ensures that expenditures align with organizational objectives.")
+    print()
+    print("Overall, the objective of an expense tracker is to empower individuals and organizations to make informed financial decisions, optimize their spending habits, \nand achieve their financial goals.")
+    print()
+    input('PRESS ENTER START EXPENSE TRACKER')
+func()
+print()
+time.sleep(2)
 
-
+No_of_Expenses=int(input("Enter the number of Expenses = "))
 for i in range(No_of_Expenses):
     print("Enter the Date of Payment")
     expense()
